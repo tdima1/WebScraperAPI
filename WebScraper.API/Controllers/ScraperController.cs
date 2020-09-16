d07bd67dbb1046de0f31845f4d142e4db009bc0b
Dima Tiberiu <dima.tiberiu1@gmail.com>
2020-09-16 21:44:42 +0300

    Add project files.

---
+++ WebScraper.API/Controllers/ScraperController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebScraper.API.Controllers
+{
+   [Route("api/[controller]")]
+   [ApiController]
+   public class ScraperController : ControllerBase
+   {
+      WebScraper scraper = new WebScraper();
+
+      // GET: api/Scraper
+      [HttpGet]
+      public async Task<IEnumerable<string>> Get()
+      {
+         await scraper.Crawl();
+         return new string[] { "value1", "value2" };
+      }
+
+      // GET: api/Scraper/5
+      [HttpGet("{id}", Name = "Get")]
+      public string Get(int id)
+      {
+         return "value";
+      }
+
+      // POST: api/Scraper
+      [HttpPost]
+      public void Post([FromBody] string value)
+      {
+         
+      }
+
+      // PUT: api/Scraper/5
+      [HttpPut("{id}")]
+      public void Put(int id, [FromBody] string value)
+      {
+      }
+
+      // DELETE: api/ApiWithActions/5
+      [HttpDelete("{id}")]
+      public void Delete(int id)
+      {
+      }
+   }
+}
---
+++ WebScraper/WebScraper.cs
@@ -0,0 +1,25 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScraper
+{
+   public class WebScraper
+   {
+      public async Task Crawl()
+      {
+         var url = "https://www.emag.ro/televizoare/brand/wellington/c?ref=bc";
+         var httpClient = new HttpClient();
+         var html = await httpClient.GetStringAsync(url);
+         var htmlDocument = new HtmlDocument();
+
+         htmlDocument.LoadHtml(html);
+         var divs = htmlDocument.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Contains("card-section-wrapper")).ToList();
+
+      }
+   }
+}
