b051842edb05526e655df931861bb600df82d8e9
Dima Tiberiu <dima.tiberiu1@gmail.com>
2020-09-16 23:45:15 +0300

    Added functionality to get some basic data from emag.ro. Seems like there needs to be a unique crawler method for every site the app supports.

--- WebScraper/WebScraper.cs
+++ WebScraper/WebScraper.cs
@@ -18,7 +18,25 @@ namespace WebScraper
          var htmlDocument = new HtmlDocument();
 
          htmlDocument.LoadHtml(html);
-         var divs = htmlDocument.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Contains("card-section-wrapper")).ToList();
+         List<HtmlNode> divsCardSectionMid = htmlDocument.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Contains("card-section-mid")).ToList();
+         List<HtmlNode> divsCardSectionBtm = htmlDocument.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Contains("card-section-btm")).ToList();
+         
+         try {
+            foreach(var div in divsCardSectionMid) {
+               var name = div.Descendants("a").FirstOrDefault().ChildAttributes("title").FirstOrDefault().Value;
+               var link = div.Descendants("a").FirstOrDefault().ChildAttributes("href").FirstOrDefault().Value;
+            }
+
+            foreach(var div in divsCardSectionBtm) {
+               var oldPrice = div.Descendants("div").Where(node => node.GetAttributeValue("class", "").Contains("pricing-old")).FirstOrDefault();//.FirstChild.GetDirectInnerText();
+               string newPrice = div.Descendants("p").Where(node => node.GetAttributeValue("class", "").Contains("new-price")).FirstOrDefault().FirstChild.GetDirectInnerText();
+               var atr = div.Attributes;
+            }
+         } catch(Exception e) {
+
+            throw e;
+         }
+
 
       }
    }
